c8a2f897e58488bfe660c5d87c76d4071a316af9
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-03 16:44:10 +0300

    Adds blogs get method

---
+++ Blog.Business/Services/BlogService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using Blog.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Business.Services
+{
+    public class BlogService : IBlogService
+    {
+        private IUnitOfWork unit;
+        private IMapper mapper;
+
+        public BlogService(IUnitOfWork unit, IMapper mapper)
+        {
+            this.unit = unit;
+            this.mapper = mapper;
+        }
+
+        public async Task Create(BlogModel model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<BlogActivityInfoModel>> GetByBiggestActivityInPeriod(DateTime oldest, DateTime latest)
+        {
+            var blogs = await unit.BlogRepository.GetAndIncludeAsync(
+                predicate: x => x.Posts.Any(x => oldest < x.Publication && x.Publication < latest),
+                x => x.Posts, x => x.Author);
+
+            var models = new List<BlogActivityInfoModel>();
+
+            foreach (var blog in blogs)
+            {
+                var activeUsers = await unit.UserRepository.GetAsync(user => blog.Posts.Any(post => post.AuthorId != null && post.AuthorId == user.Id));
+
+                var model = new BlogActivityInfoModel
+                {
+                    Blog = mapper.Map<BlogListItemModel>(blog),
+                    PostsCount = blog.Posts.Count(),
+                    UsersCount = activeUsers.Count()
+                };
+
+                models.Add(model);
+            }
+
+            return models;
+        }
+
+        public async Task<BlogModel> GetById(int id)
+        {
+            var blog = await unit.BlogRepository.GetByIdAsync(id);
+
+            return mapper.Map<BlogModel>(blog);
+        }
+    }
+}
