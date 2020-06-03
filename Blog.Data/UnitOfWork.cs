354e0a5b7c3565e6e956e1af855b9d02298774b6
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-03 16:42:55 +0300

    Adds unit of work

---
+++ Blog.Data/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private BlogContext context;
+
+        public UnitOfWork(BlogContext context, IRepository<User> userRepository, IRepository<Blog> blogRepository, IRepository<Post> postRepository)
+        {
+            this.context = context;
+            UserRepository = userRepository;
+            BlogRepository = blogRepository;
+            PostRepository = postRepository;
+        }
+
+        public IRepository<User> UserRepository { get; }
+
+        public IRepository<Blog> BlogRepository { get; }
+
+        public IRepository<Post> PostRepository { get; }
+
+
+
+        public async Task SaveChangesAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
