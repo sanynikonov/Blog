1ad4d1c3da6d1e86674c9ccd2af279ab675a1369
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-05 17:38:42 +0300

    Creates post viewmodel

---
+++ Blog.Presentation/Models/PostFormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Presentation.Models
+{
+    public class PostFormViewModel
+    {
+        [Required]
+        public int BlogId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Content { get; set; }
+    }
+}
