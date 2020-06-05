cc18cad1fa19dffd7565f37c356ff669a9a98a28
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-05 17:37:47 +0300

    Adds post validation

--- Blog.Business/Services/PostService.cs
+++ Blog.Business/Services/PostService.cs
@@ -20,12 +20,22 @@ namespace Blog.Business
 
         public async Task AddToBlog(PostModel model, int blogId)
         {
-            var blog = await unit.BlogRepository.GetByIdAndIncludeAsync(blogId, x => x.Posts);
+            var blog = await unit.BlogRepository.GetByIdAsync(blogId);
 
             if (blog == null)
-                throw new InvalidOperationException($"Blog with id {blogId} does not exist.");
+                throw new ArgumentException($"Blog with id {blogId} does not exist.");
+
+            if (model.AuthorId == null)
+                throw new ArgumentNullException($"Author id was null.");
+
+            var author = await unit.UserRepository.GetByIdAsync(model.AuthorId.Value);
+
+            if (author == null)
+                throw new ArgumentException($"User with id {model.AuthorId.Value} does not exist.");
 
             var post = mapper.Map<Post>(model);
+
+            post.BlogId = blog.Id;
 
             blog.Posts.Add(post);
 
