ccfa95956e620a18396f3cb69922524d32e74e42
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-13 11:33:43 +0300

    Adds migration with new user properties

--- Blog.Data/Migrations/20200613083117_AddUserFirstLastName.Designer.cs
+++ Blog.Data/Migrations/20200613083117_AddUserFirstLastName.Designer.cs
@@ -4,14 +4,16 @@ using Blog.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Blog.Data.Migrations
 {
     [DbContext(typeof(BlogContext))]
-    partial class BlogContextModelSnapshot : ModelSnapshot
+    [Migration("20200613083117_AddUserFirstLastName")]
+    partial class AddUserFirstLastName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
