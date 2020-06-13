23647fb1ee5350ed2a89d14c8ef457736f023ab4
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-13 10:42:12 +0300

    Creates user service

---
+++ Blog.Business/Interfaces/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Business
+{
+    public interface IUserService
+    {
+        Task<SignInResult> Login(LoginModel model);
+        Task<IdentityResult> Register(RegisterModel model);
+    }
+}
---
+++ Blog.Business/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Business.Services
+{
+    public class UserService : IUserService
+    {
+        private IUnitOfWork unit;
+
+        public UserService(IUnitOfWork unit)
+        {
+            this.unit = unit;
+        }
+
+        public async Task<SignInResult> Login(LoginModel model)
+        {
+            return await unit.SignInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+        }
+
+        public async Task<IdentityResult> Register(RegisterModel model)
+        {
+            var user = new User
+            {
+                UserName = model.Email,
+                Email = model.Email,
+                FirstName = model.FirstName,
+                LastName = model.LastName
+            };
+
+            return await unit.UserManager.CreateAsync(user, model.Password);
+        }
+    }
+}
