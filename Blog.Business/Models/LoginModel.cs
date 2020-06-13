fb1f0784f98c5347166b70d4070ad9be2b741cb7
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-13 10:36:36 +0300

    Adds register and login models

---
+++ Blog.Business/Models/LoginModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Blog.Business
+{
+    public class LoginModel
+    {
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ Blog.Business/Models/RegisterModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Blog.Business
+{
+    public class RegisterModel
+    {
+        [Required, DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false)]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false)]
+        public string LastName { get; set; }
+    }
+}
