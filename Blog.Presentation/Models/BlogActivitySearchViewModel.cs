1efe25577cc6acc3987c7b8d524a34218cf8fa1e
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-06-23 10:41:33 +0300

    Creates view model for blog activity

---
+++ Blog.Presentation/Models/BlogActivitySearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blog.Business;
+
+namespace Blog.Presentation.Models
+{
+    public class BlogActivitySearchViewModel
+    {
+        public DateTime Oldest { get; set; }
+        public DateTime Latest { get; set; }
+        public IEnumerable<BlogActivityInfoModel> Blogs { get; set; }
+
+        public BlogActivitySearchViewModel()
+        {
+            Blogs = new List<BlogActivityInfoModel>();
+        }
+    }
+}
